9adeaa43ec175da12c7b72ab9ec449c5ff6f9bcd
NitishNayan <nitishnayan26@gmail.com>
2024-06-11 00:13:32 +0530

    Update Demo1.java

--- Automation_Lead/src/forkdemo/Demo1.java
+++ Automation_Lead/src/forkdemo/Demo1.java
@@ -3,7 +3,7 @@ package forkdemo;
 public class Demo1
 {
 	public static void main(String[] args) {
-		System.out.println("this demo class");
+		System.out.println("this demo class added by nitish on Monday");
 	}
 
 }
